afd3784cc7ac42edd7687337922414558978f534
Rast <thawekyawsitt@gmail.com>
2026-04-12 00:59:46 +0700

    Fix Vercel function export

--- api/vercel.mjs
+++ api/vercel.mjs
@@ -1,3 +1,6 @@
+import { createRequire } from "node:module";
+
+const require = createRequire(import.meta.url);
 const { createAlbumServer } = require("../server");
 
 const { server } = createAlbumServer({
@@ -5,10 +8,10 @@ const { server } = createAlbumServer({
   port: 0,
 });
 
-module.exports = (req, res) => {
+export default function handler(req, res) {
   req.url = reconstructOriginalUrl(req.url);
   return server.emit("request", req, res);
-};
+}
 
 function reconstructOriginalUrl(requestUrl) {
   const url = new URL(requestUrl || "/", "http://localhost");
